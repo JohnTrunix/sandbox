0b2b59bf56ec02e76c24e7c70f9d126346248b72
JohnTrunix <john@trunix.ch>
2023-08-29 19:42:57 +0200

    test(linkedList): test Search() function

--- datastructures/linkedList/linkedList_test.go
+++ datastructures/linkedList/linkedList_test.go
@@ -29,3 +29,27 @@ func TestAdd(t *testing.T) {
 	p = p.Next
 	assert.Nil(t, p)
 }
+
+func TestSearch(t *testing.T) {
+	l := New()
+	l.Add(100)
+	l.Add(200)
+	l.Add(300)
+	l.Add(400)
+
+	r1 := l.Search(100)
+	assert.True(t, r1)
+
+	r2 := l.Search(200)
+	assert.True(t, r2)
+
+	r3 := l.Search(300)
+	assert.True(t, r3)
+
+	r4 := l.Search(400)
+	assert.True(t, r4)
+
+	r5 := l.Search(800)
+	assert.False(t, r5)
+
+}
